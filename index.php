857725706befa5546afd491e3e3860812a2d40b2
seqi <tomseqi@live.co.uk>
2020-08-08 18:12:53 +0100

    Moved count to input element

--- index.php
+++ index.php
@@ -1,55 +1,55 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="includes/css/bootstrap.min.css">
-    <title>PomfBoards - /everythinggoes/</title>
-    <script>
-      window.onload = () => {
-        (function setupCharacterCount() {
-          const MAX_LENGTH = 1000
-          const inputEl = document.getElementById('text')
-          const currentEl = document.getElementById('char-count')
-          const maxEl = document.getElementById('max-count')
+  <title>PomfBoards - /everythinggoes/</title>
+  <script>
+    window.onload = () => {
+      (function setupCharacterCount() {
+        const MAX_LENGTH = 1000
+        const inputEl = document.getElementById('text')
+        const countEl = document.getElementById('char-count')
 
-          maxEl.innerHTML = MAX_LENGTH
+        countEl.value = MAX_LENGTH
 
-          inputEl.addEventListener('input', (evt) => {
-            currentEl.innerHTML = evt.target.value.length
-          })
-        })()
-      }
-    </script>
-  </head>
-  <body>
-  	<div class="container-fluid">
+        inputEl.addEventListener('input', (evt) => {
+          countEl.value = MAX_LENGTH - evt.target.value.length
+        })
+      })()
+    }
+  </script>
+</head>
+
+<body>
+  <div class="container-fluid">
     <center><h1>/everythinggoes/</h1></center>
     <center><p>Alpha V.0.0.0.0.0.1 @ <a href="https://github.com/nokonoko/Pomf-Board">Github</a></p></center>
     <form action="api.php" method="POST">
-    <form class="form-inline">
-    <div class="form-group">
-      <input type="name" class="form-control" id="name" name="name" placeholder="Anon">
-    </div>
-    <div class="form-group">
-      <textarea class="form-control" rows="5" id="text" name="text" placeholder="Max 1000 chars" maxlength="1000"></textarea>
-    </div>
+      <form class="form-inline">
+        <div class="form-group">
+          <input type="name" class="form-control" id="name" name="name" placeholder="Anon">
+        </div>
+        <div class="form-group">
+          <input maxlength="4" size="4" disabled id="char-count"></input>
+          <textarea class="form-control" rows="5" id="text" name="text" placeholder="Max 1000 chars" maxlength="1000"></textarea>
+        </div>
         <button type="submit" class="btn btn-default">Post!</button>
-        <span>
-          <span id="char-count">0</span>/<span id="max-count"></span>
-        </span>
-  </form><br>
+      </form><br>
 
-	<?php
-	if(PHP_ERRORS == 'true'){
-  ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-  }
-	require_once('includes/php/core.php');
-	GetPost();
-	?>
-	</div>
-	</body>
+<?php
+if(PHP_ERRORS == 'true'){
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+}
+require_once('includes/php/core.php');
+GetPost();
+?>
+  </div>
+</body>
+
 </html>
